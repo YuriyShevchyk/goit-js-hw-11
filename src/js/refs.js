24bed4cc69e069fd3a6964f9492769da61a0bc79
YuriyShevchyk <97519385+YuriyShevchyk@users.noreply.github.com>
2022-10-14 14:13:13 +0300

    fetch.js add

---
+++ src/js/refs.js
@@ -0,0 +1,10 @@
+const refs = {
+    input: document.querySelector('input'),
+    form: document.querySelector('.search-form'),
+    btnLoad: document.querySelector('.load-more'),
+    gallery: document.querySelector('.gallery'),
+    alert: document.querySelector('.alert')
+
+}
+
+export default refs;
