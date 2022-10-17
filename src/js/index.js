35edf3b1ca174361eb1d25a05bee11ff1cbf1538
YuriyShevchyk <97519385+YuriyShevchyk@users.noreply.github.com>
2022-10-17 16:09:20 +0300

    css added

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,10 @@
-import '..css/styles.css';
+import '../css/styles.css';
 import getRefs from "./refs";
+import fetchPhoto from './fetchphoto';
+import markup from './markup';
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 
 
