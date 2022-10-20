01ebf276f92751c1572a369eaf114f31d495d406
YuriyShevchyk <97519385+YuriyShevchyk@users.noreply.github.com>
2022-10-20 19:01:16 +0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -8,13 +8,9 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 const refs = getRefs();
 
-// refs.btnLoad.classList.add('is-hidden');
-
-refs.form.addEventListener('submit', onSearch);
-refs.btnLoad.addEventListener('click', onLoad);
-
 let pageNumber = 1;
 let searchQuery ="";
+refs.btnLoad.classList.add('is-hidden');
 const lightbox = new SimpleLightbox('.gallery a', 
 {
     captionsData:'alt',
@@ -22,36 +18,71 @@ const lightbox = new SimpleLightbox('.gallery a',
     captionDelay: 250,
 });
 
+
+refs.form.addEventListener('submit', onSearch);
+refs.btnLoad.addEventListener('click', onLoad);
+
+
 function onSearch(evt) {
     evt.preventDefault();
-    searchQuery = evt.currentTarget.elements.searchQuery.value.trim();
-    console.log(searchQuery);
-    if (searchQuery === 0) {
-        return;
-    } else {
-        clearGallery();
-        pageNumber = 1;
-        fetchPhoto(pageNumber, searchQuery);
+
+    clearGallery();
+
+    if (pageNumber > 0) {
+        page = 1;
     }
-}
+    refs.btnLoad.classList.add('is-hidden');
+    searchQuery = evt.currentTarget.elements.searchQuery.value.trim().toLowerCase();
+    console.log(searchQuery);
+    // if (searchQuery === 0) {
+    //     return;
+    // } else {
+        // clearGallery();
+        // pageNumber = 1;
+        onFetch();
+        console.log(onFetch);
+        evt.target.reset();
+    }
+
 
 function onLoad() {
     refs.btnLoad.classList.add('is-hidden');
+    lightbox.refresh();
     pageNumber +=1;
-    searchQuery = refs.input.value.trim();
-    fetchPhoto(pageNumber, searchQuery);
+    // searchQuery = refs.input.value.trim();
+    // fetchPhoto(pageNumber, searchQuery);
+    onFetch();
 }
-
-function insertMarkup(img) {
-    const result = createGallery(img);
-    refs.gallery.insertAdjacentElement('beforeend', result);
-
+function insertMarkup (el) {
+    refs.gallery.insertAdjacentHTML("beforeend", markup(el));
 }
-
-function createGallery(img) {
-    return img.reduce((acc, item) => acc + markup(item),"")
+function createMarkup(img) {
+    img.forEach(el => {
+        insertMarkup(el);
+    })
 }
+// function insertMarkup(img) {
+//     const result = createGallery(img);
+//     refs.gallery.insertAdjacentElement('beforeend', result);
+
+// }
+
+// function createGallery(img) {
+//     return img.reduce((acc, item) => acc + markup(item),"")
+// }
 
 function clearGallery () {
     refs.gallery.innerHTML = "";
+}
+
+function onFetch() {
+    fetchPhoto(searchQuery, pageNumber)
+    .then(img => {
+        createMarkup(img);
+        console.log(img);
+        if (img.length === 39) {
+            refs.btnLoad.classList.remove('is-hidden');
+        }
+        lightbox;
+    })
 }
